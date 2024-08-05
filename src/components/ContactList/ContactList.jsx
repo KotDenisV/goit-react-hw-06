57efdcb75b1f6eea62325afc86c9ef1f15d4b834
KotDenisV <kot.denis@gmail.com>
2024-08-05 11:31:28 +0300

    fixed Contact

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,18 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
 import s from './ContactList.module.css';
 import { selectContacts, selectFilter } from '../../redux/selectors';
-import { deleteContacts } from '../../redux/contactsSlice';
 
 function ContactList() {
-  const contacts = useSelector(selectContacts) || [];
-  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts) || [];  
   const filter = useSelector(selectFilter) || '';
   const filteredData = contacts.filter(item => item.name.toLowerCase().includes(filter.toLowerCase()));
-  const handleDelete = (id) => {
-    dispatch(deleteContacts(id));
-  };
+  
     return (
     <ul className={s.list}>
       {filteredData.map((contact) => (
         <li className={s.item} key={contact.id}>
-          <Contact data={contact} onDelete={() => handleDelete(contact.id)} />
+          <Contact data={contact} />
         </li>
       ))}
     </ul>
